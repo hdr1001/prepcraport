e18c343c56302746df1e94cd9c5cd5ec89562b2b
Hans de Rooij <hansderooij@hotmail.com>
2022-01-10 20:35:42 +0100

    App.js no longer class based, now based on hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,34 +1,23 @@
 import './App.css';
-import React from 'react';
+import { useState, useEffect } from 'react';
 import { styled } from '@mui/material/styles';
 import AhAppBar from './AhAppBar';
 
-export default class App extends React.Component {
-   constructor(props) {
-      super(props);
+const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
 
-      this.state = {
-         arrFiles: []
-      };
+export default function App(props) {
+   const [arrFiles, setArrFiles] = useState([]);
 
-      this.handleFileInp = this.handleFileInp.bind(this);
-   }
+   useEffect(() => console.log('arrFiles = ' + arrFiles.map(file => file.name).join(', '), [arrFiles]));   
 
-   handleFileInp = e => {
-      this.setState(
-         { arrFiles: this.state.arrFiles.concat(Array.from(e.target.files)) }
-      )
-   }
+   const handleFileInp = e => {
+      setArrFiles([...arrFiles, ...Array.from(e.target.files)])
+   };
 
-   render() {
-      const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
-
-      return (
-         <>
-            <AhAppBar handleFileInp={this.handleFileInp} />
-
-            <Offset />
-         </>
-      );
-   }
+   return (
+      <>
+         <AhAppBar handleFileInp={handleFileInp} />
+         <Offset />
+      </>
+   );
 }
